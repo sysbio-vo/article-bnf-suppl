b89eb5821eca2c3832b949e0b5cb623d2d2a5fb2
aln <alnfgit@gmail.com>
2018-01-04 01:27:53 +0200

    working netbenchmark for dream4 data

--- scripts/netben.R
+++ scripts/netben.R
@@ -1,73 +1,77 @@
 library(netbenchmark)
-
-
-# Define a wrapper function
-Spearmancor <- function(data, regulators){
-  regulators = 1
-  cor(data,method="spearman")
-}
-## Not run:
-# Register it to all.fast methods
-RegisterWrapper("Spearmancor")
-# Register it to all methods
-RegisterWrapper("Spearmancor", all.fast=FALSE)
-
-
-
-top20 <- netbenchmark(seed=42, datasets.num=1, experiments = 2001)
-top20.fast.list[[1]]
-
-View(rogers1000.data)
-View(rogers1000.net)
-
-View(gnw2000.net)
-
-Yeast.TFs <- read.table("RegulationTwoColumnTable_Documented_2013927.tsv",
-                          check.names = FALSE, header=FALSE, sep=";", quote="")
-
-Yeast.TFs <- levels(unique(Yeast.TFs[,1]))
-
+library(networkBMA)
 library(igraph)
-g <- graph.adjacency(gnw2000.net)
-edg <- get.edgelist(g)
-
-
 library(org.Sc.sgd.db)
+library(abind)
+data(dream4)
+data(vignette)
+
+
+# Read Yeast TFs
+Yeast.TFs <- read.table("../data/RegulationTwoColumnTable_Documented_2013927.tsv",
+                        check.names = FALSE, header=FALSE, sep=";", quote="")
+Yeast.TFs <- levels(unique(Yeast.TFs[,1]))
 gene_to_ORF <- select(org.Sc.sgd.db, keys=Yeast.TFs, columns = c("ORF"),
                       keytype="COMMON")
 
-genes <- unique(edg[,1])
 
-length(which(gene_to_ORF$ORF %in% genes))
+# Define a wrapper function
+ScanBMA <- function(data){
+  edges <- networkBMA(data = data, nTimePoints = nrow(data))
+  g <- graph.data.frame(edges)
+  adj <- get.adjacency(g, attr='PostProb',sparse=FALSE)
+  return(adj[,colnames(data)])
+}
 
-library(gsubfn)
-pat <- read.pattern("TFConsensusList_20130918.transfac", pattern="^NA + (\\S+)")
+# Register it to all.fast methods
+RegisterWrapper("ScanBMA")
+# Register it to all methods
+RegisterWrapper("ScanBMA", all.fast=FALSE)
 
+benchmark <- function(data, true.net, methods, sym=TRUE, no.top=100) {
+  auroc <- netbenchmark.data(data=data, eval="AUROC", methods=methods, sym = sym,
+                             no.topedges = no.top, true.net = true.net)
+  aupr <- netbenchmark.data(data=data, methods=methods, sym = sym,
+                          no.topedges = no.top, true.net = true.net)
+  tp <- netbenchmark.data(data=data, eval="no.truepos", methods=methods, sym = sym,
+                           no.topedges = no.top, true.net = true.net)
+      
+  result <- rbind(auroc[[1]], aupr[[1]], tp[[1]])
+  cpu <- rbind(auroc[[2]], aupr[[2]], tp[[2]])
+  cpu <- as.data.frame(t(colMeans(cpu)))
+  cpu$rand = 0
+  result <- rbind(result, cpu)
+  rownames(result) <- c("AUROC", "AUPR", "TP", "Time")
+  return(result)
+}
 
-Lines <- "this is the first field 1 2
-more text 3 4
-"
-pat <- "^(.*) +(\\S+) +(\\S+)$"
-read.pattern(text = Lines, pattern = pat, as.is = TRUE)
+methods <- c("ScanBMA", "aracne.wrap","c3net.wrap","clr.wrap",
+                 "Genie3.wrap","mrnet.wrap",
+                 "mutrank.wrap","mrnetb.wrap","pcit.wrap")
+    
+#methods <- c("clr.wrap", "ScanBMA")
+#methods <- c("GeneNet.wrap")
 
+dream.bench <- function(data, gold, methods) {
+  results <- c()
+  for (network in 1:length(data)) {
+    dat <- data[[network]][, -c(1:2)]
+    true.net <- gold[[network]]
+    true.net <- graph.data.frame(true.net)
+    true.net <- get.adjacency(true.net, attr='edge',sparse=FALSE)
+    true.net <- as.matrix(true.net[colnames(dat), colnames(dat)])
+    result <- benchmark(dat, true.net, methods)
+    results <- c(results, list(result)) 
+  }
 
+  return(results)
+  arr <- abind(results, along=3)
+  average <- rowMeans(arr, dims = 2)
+  
+  return(average[[1]])    
+}
+  
+result <- dream.bench(dream4ts10, dream4gold10, methods)
+View(result)
 
-library(networkBMA)
-data(dream4)
-dream4ts10[[1]][,-(1:2)]
-dream4ts10[[1]][,3]
-
-data(vignette)
-ScanBMA()
-networkBMA()
-
-head(brem.data)
-
-network=1
-nTimePoints <- length(unique(dream4ts10[[network]]$time))
-edges1ts10 <- networkBMA( data = dream4ts10[[network]][,-(1:2)], 
-                          nTimePoints = nTimePoints, prior.prob = 0.01)
-summary(edges1ts10)
-
-ctables <- contabs.netwBMA(edges1ts10, dream4gold10[[network]],
-                           reg.known, thresh=c(.5,.75,.9))
+system("./test.sh")
