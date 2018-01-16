5a4de7e07b2479ba95e2b5d67f8fa2da60e5bae8
aln <alnfgit@gmail.com>
2018-01-16 02:59:20 +0200

    added tests of different packages, which calculate area under curve metrics

---
+++ scripts/test_metrics.R
@@ -0,0 +1,66 @@
+## Testing netbenchmark
+
+library(netbenchmark)
+
+ngenes <- dim(syntren300.data)[2]
+nlinks <- ngenes^2-ngenes
+no.topedges=100
+no.edges <- round(nlinks*no.topedges/100)
+
+# True net versus true net
+r1 <- evaluate(syntren300.net,syntren300.net,extend=no.edges,sym=FALSE)
+aupr(r1)
+auroc(r1)
+r2 <- evaluate(syntren300.net,syntren300.net,extend=0,sym=FALSE)
+aupr(r2)
+auroc(r2)
+
+# True net with 0.9 instead of zeros versus true net
+syntren300.net.nozeros <- syntren300.net
+syntren300.net.nozeros[syntren300.net.nozeros==0] <- 0.9
+
+r3 <- evaluate(syntren300.net.nozeros,syntren300.net,extend=0,sym=FALSE)
+aupr(r3)
+auroc(r3)
+r4 <- evaluate(syntren300.net.nozeros,syntren300.net,extend=no.edges,sym=FALSE)
+aupr(r4)
+auroc(r4)
+
+## Testing minet
+
+library(minet)
+
+# True net versus true net
+e1 <- validate(syntren300.net,syntren300.net)
+auc.pr(e1)
+auc.roc(e1)
+
+# True net with 0.9 instead of zeros versus true net
+e2 <- validate(syntren300.net.nozeros,syntren300.net)
+auc.pr(e2)
+auc.roc(e2)
+
+## Testing networkBMA
+
+library(networkBMA)
+library(igraph)
+
+# True net versus true net
+g <- graph.adjacency(syntren300.net, weighted=TRUE, mode="undirected")
+true <- as.data.frame(get.edgelist(g))
+true$weight <- E(g)$weight
+
+contingencyTables <- contabs(network = true, reference = true[,1:2],
+                             size = no.edges)
+prc(contingencyTables, plotit = FALSE)
+roc(contingencyTables, plotit = FALSE)
+
+# True net with 0.9 instead of zeros versus true net
+g <- graph.adjacency(syntren300.net.nozeros, weighted=TRUE, mode="undirected")
+inf <- as.data.frame(get.edgelist(g))
+inf$weight <- E(g)$weight
+
+contingencyTables <- contabs(network = inf, reference = true[,1:2],
+                             size = no.edges)
+prc(contingencyTables, plotit = FALSE)
+roc(contingencyTables, plotit = FALSE)
